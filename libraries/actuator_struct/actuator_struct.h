8effe20eb8c462265361425be4b1e678b4ec671f
Jmack66 <mack.jonah@gmail.com>
2022-09-21 18:16:07 +0100

    smooth write edits

--- libraries/actuator_struct/actuator_struct.h
+++ libraries/actuator_struct/actuator_struct.h
@@ -24,17 +24,21 @@ struct ActuatorGroup : Array<T,Size> {
     }
 
     //Servo Only 
-    void smoothWrite(int actuator_number, float pos, float number_of_interps){
+    bool smoothWrite(int actuator_number, float pos, float number_of_interps){
         float increment = 1.0 / number_of_interps; 
         if(smooth_write_interp == 0){
             init_value = a.data[actuator_number].read();
         }
         auto current_pos = sk_math::SMOOTLERP(init_value, pos, smooth_write_interp); 
+        if(sk_math::ISCLOSE(current_pos,pos,0.01)){
+            return true; 
+        }
         smooth_write_interp += increment;
         a.data[actuator_number].write(current_pos);
         if(smooth_write_interp > 1.0){
             smooth_write_interp = 0;
         }
+        return false;
     }
 
     void attach(int* pins){
