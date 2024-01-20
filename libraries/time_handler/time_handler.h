1c68f8bc21d33705ca6862d2750183af933314fc
Jmack66 <33293161+Jmack66@users.noreply.github.com>
2024-01-20 17:59:34 +0000

    Update time_handler.h to fix same name issue

--- libraries/time_handler/time_handler.h
+++ libraries/time_handler/time_handler.h
@@ -4,22 +4,21 @@
 #include "utility.h"
 
 struct Timer {
-    uint32_t cycles = 0;
-    uint32_t start_t = 0;
-    uint32_t finish_t = 0;
-    float delta_t = 0;
+    float cycles = 0;
+    float t_start = 0;
+    float t_finish = 0;
 
     Timer() {}
-    void start() { start_t = micros(); }
-    void stop() { finish_t = micros(); }
+    void start() { t_start = micros(); }
+    void stop() { t_finish = micros(); }
     float deltaT() {
-        if (finish_t == 0) {
+        if (t_finish == 0) {
             stop();
         }
-        return (finish_t - start_t) / 1000000.0f;
+        return (t_finish - t_start) / 1000000.0f;
     }
     void count() { cycles++; }
-    uint32_t getCycles() { return cycles; }
-    uint32_t getUpTime() { return micros() / 1000000.0; }
-    uint32_t getTime() { return (micros() - start_t) / 1000000.0; }
+    float getCycles() { return cycles; }
+    float getUpTime() { return micros() / 1000000.0; }
+    float getTime() { return (micros() - t_start) / 1000000.0; }
 };
