6c58bd8e29c8c798f665715db6f6eb461ce5c701
Jmack66 <mack.jonah@gmail.com>
2024-06-28 23:18:58 +0100

    TVC tester

--- Source/Source.ino
+++
@@ -1,9 +0,0 @@
-#include "internal.h"
-#include "quat_vec.h"
-void setup() {
-sm::SM_INIT();
-  }
-void loop() 
-{
-sm::SM_UPDATE_LOOP();
-}
--- Source/sub_task.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "utility.h"
-// This is where we place long functions or repeated code
-
-namespace subtask {
-
-}  // namespace subtask
