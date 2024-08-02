8fa92c4c09d6d866bf335d37e475d8109258ebe4
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2024-08-03 00:33:28 +0100

    Fixed?
    Please test this fix yourself, but it is working for me!

--- Source/task.h
+++ Source/task.h
@@ -11,51 +11,55 @@
 
 #include "orientation.h"
 
-// Tasks go here-- this keeps the main file clean and focused on the "flow" of tasks
-// Our general functions will be defined here things like our main loops
-// Long sections of code or repeated code loops can be moved to sub_task.h
-
-// The functions in place here can be changed to suit your needs
-// The ones listed here serve as inspiration--feel free to change them as
-// you need -- but remember to change your Tasks in main.h
-
 namespace task {
+
+    // I have commented my changes, sorry if you didnt want it pushed but it works so
+    // enjoy testing it yourself - I think the problems were:
+    // 1) You didn'd convert the bmi inputs to to radians
+    // 2) The way you calculated delta time
+    // 3) The sensor input was being maxed out consistently
 
     Timer timer;
     Orientation ori;
-    // Globals can be defined here
-    // Can be used for code that only runs once
-    // This can also be run multiple times by changing the code flow in main.h
+
     void Setup() {
-        PRINTLN("ASDAF");
         sensors::init();
 
     }
 
-    // Can be used to automatically test actuators
-    // Very useful for quick plug and play testing
     void ActuatorTest() {
-        PRINTLN("ASDAF");
 
     }
 
-    // Can be used to print sensor values and any other required calibration
     void Calibration() {
-        PRINTLN("ASDAF");
-
+        // Make sure the first iteration doesnt mess anything up with a large delta t
+        timer.start();
+        timer.stop();
     }
 
-    // Code that loops
     void Loop() {
+        // Moved around timer because the way you did it didnt make sense
+        float dt = timer.deltaT();
         timer.start();
-        auto gyro = sensors::getGyro();
-        auto ypr = ori.gyroToYpr(gyro, timer.deltaT());
+
+        // Changed sensor range to 2000 as it was capping out at 125 (bmi088.h)
+        // Changed dt to positive because why the hell was it negative lmfao (oritentation.h)
+        auto gyro = sensors::getGyro() * DEG2RAD;
+        auto ypr = ori.gyroToYpr(gyro, dt);
+
+        // IDK what this does but im guessing ur using it
         actuators::update(ypr.y, ypr.z);
-        PRINTLN(ypr.y);
+
+        // Added some more graphs because #WeLoveData
         GRAPH("x", ypr.x, TOP);
         GRAPH("y", ypr.y, TOP);
         GRAPH("z", ypr.z, TOP);
+        GRAPH("x", gyro.x, BOT);
+        GRAPH("y", gyro.y, BOT);
+        GRAPH("z", gyro.z, BOT);
         END_LOG;
+
+        // End the timer at an appropriate time
         timer.stop();
     }
 
@@ -63,4 +67,4 @@ namespace task {
 
     }
 
-}  // namespace task
+}
