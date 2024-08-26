b8906fe5cc60257ee9850fa620f5591030a659c6
Jmack66 <mack.jonah@gmail.com>
2024-08-26 23:46:03 +0100

    pin_mapping

---
+++ .github/build.py
@@ -0,0 +1,22 @@
+import sys
+import time
+import os
+import re
+import shutil
+import subprocess
+import collections
+from all_platforms import ALL_PLATFORMS
+
+
+
+
+
+
+def main():
+
+    pass
+
+
+
+if __name__ == "__main__":
+
---
+++ libraries/stem/pin_mapping.h
@@ -0,0 +1,43 @@
+#pragma once 
+
+/*
+    definitions for the servo multiplexer pins on STEM
+
+*/
+
+#define ST0 0  // Steppers
+#define ST1 1
+#define ST2 2
+#define ST3 3
+
+#define PS1 7  // Servos
+#define PS2 6
+#define PS3 5
+#define PS4 4
+#define PS5 15
+#define PS6 14
+#define PS7 13
+#define PS8 12
+#define PS9 8
+#define PS10 9
+#define PS11 10
+#define PS12 11
+
+
+/*
+    multi pin is pwm pin + 4
+    pwm  board
+    3 is 1 
+    2 is 2 
+    1 is 3 
+    0 is 4
+    11 is 5
+    10 is 6
+    9 is 7
+    8 is 8
+    4 is 9
+    5 is 10
+    6 is 11
+    7 is 12
+*/
+
